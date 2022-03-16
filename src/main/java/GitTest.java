9f4ecd86bc2e224c0351aab10fb1ed66516f17b9
qingshui <1664736954@qq.com>
2022-03-16 18:37:48 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,9 @@
+/**
+ * @authour shuangliang
+ * @date 2022/3/15 - 23:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
